cc8ea6c04bebffa2a0f74e56790798cf0696fe65
Molystand <molystand@gmail.com>
2019-03-15 07:19:23 +0300

    Изменение новости и тега (формы и миксины)

--- news/urls.py
+++ news/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('', news_list, name='news_list_url'),
     path('news/create/', NewsCreate.as_view(), name='news_create_url'),
     path('news/<str:slug>/', NewsDetail.as_view(), name='news_detail_url'),
+    path('news/<str:slug>/update/', NewsUpdate.as_view(), name='news_update_url'),
     path('tags/', tags_list, name='tags_list_url'),
     path('tag/create/', TagCreate.as_view(), name='tag_create_url'),
     path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
-
+    path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 from .models import News, Tag
-from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin
 from .forms import TagForm, NewsForm
 
 
@@ -23,6 +23,12 @@ class NewsCreate(ObjectCreateMixin, View):
     template = 'news/news_create_form.html'
 
 
+class NewsUpdate(ObjectUpdateMixin, View):
+    model = News
+    model_form = NewsForm
+    template = 'news/news_update_form.html'
+
+
 def tags_list(request):
     """Все теги
     """
@@ -38,3 +44,22 @@ class TagDetail(ObjectDetailMixin, View):
 class TagCreate(ObjectCreateMixin, View):
     model_form = TagForm
     template = 'news/tag_create_form.html'
+
+
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'news/tag_update_form.html'
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(instance=tag)
+    #     return render(request, 'news/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+    #
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(request.POST, instance=tag)
+    #
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     return render(request, 'news/tag_update_form.html', context={'form': bound_form, 'tag': tag})
